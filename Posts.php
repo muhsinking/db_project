0db28954613fc0af04c9aafda1d1b432b9f67497
Philip Franchi <phil@Philips-Macbook.local>
2015-04-28 00:21:21 -0400

    Comment functionality WIP

--- Posts.php
+++ Posts.php
@@ -10,7 +10,7 @@
             
             if($num_rows > 0) {
                 echo "<table border=\"1\">\n";
-                echo "  <tr><th>ID</th><th>Title</th><th>Content/th></tr>\n";
+                echo "  <tr><th>Number</th><th>Title</th><th>Content</th></tr>\n";
                 for($i = 0; $i < $num_rows; $i++) {
                     $post = mysqli_fetch_assoc($query);
                     
@@ -18,16 +18,18 @@
                     $Title = $post['Title'];
                     $Content = $post['Content'];
                     
-                    echo "<tr><td>$ID</td><td>$Title</td><td>";
-                    echo "$Content</td><td><a href=\"DeletePost.php?ID=$ID\">Delete</a></td>";
-                    echo "<td><a href=\"EditPost1.php?ID=$ID&Title=$Title&Content=$Content\">Edit</a></tr>\n";
+                    echo "<tr><td>$Number</td><td>$Title</td>";
+                    echo "<td>$Content</td>";
+                    echo "<td><a href=\"Comments.php?ID=$Post\">Comments</a></td>";
+                    echo "<td><a href=\"DeletePost.php?ID=$Number\">Delete</a></td>";
+                    echo "<td><a href=\"EditPost1.php?Number=$Number&Title=$Title&Content=$Content\"> Edit </a></tr>\n";
                 }                
                 echo "</table><br/>\n";
                 echo "<h3>Add Post</h3>\n";
                 echo "<form action=\"AddPost.php\" method=\"get\">\n";
                 echo "  <table>\n";
-                echo "    <tr><th>First Name:</th><td><input type=\"text\" name=\"Title\"/></td></tr>\n";
-                echo "    <tr><th>Last Name:</th><td><input type=\"text\" name=\"Content\"/></td></tr>\n";
+                echo "    <tr><th>Title:</th><td><input type=\"text\" name=\"Title\"/></td></tr>\n";
+                echo "    <tr><th>Content:</th><td><input type=\"text\" name=\"Content\"/></td></tr>\n";
                 echo "  </table>\n";
                 echo "<input type=\"submit\" name=\"submit\" value=\"Add Post\"/>";
                 echo "</form>\n";
